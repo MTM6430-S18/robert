03eea0e20b85b8769e8d33303ce7f74fdc8be3be
Robert McKenney <robert@mckenney.ca>
2018-05-22 10:32:11 -0400

    Add exercises: week1 and week2

---
+++ exercises/week2/main.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'hello-world',
+  components: {},
+  props: ['sender'],
+  data: () => ({
+    message: 'Greetings form the planet Earth!'
+  }),
+  computed: {},
+  methods: {},
+  // component lifecyle event handlers
+  created() {},
+  mounted() {}
+  // ...
+}
